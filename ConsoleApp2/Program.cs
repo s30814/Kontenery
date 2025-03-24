a0122cddb6798a4d3a76385336255494392048c9
s30814 <s30814@pjwstk.edu.pl>
2025-03-24 23:49:27 +0100

    male poprawki

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace ContainerManagementSystem
-{
+﻿
     public abstract class Kontener
     {
         public string NumerSeryjny { get; }
@@ -269,4 +264,3 @@ namespace ContainerManagementSystem
             ship.WyswietlInfoStatku();
         }
     }
-}
